3f1efa82a5ffa9f202e46a5afa1d6db34f6c8b83
rikster-r <seitdanial43@gmail.com>
2025-05-17 20:43:11 +0500

    Cleanup initial state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Geist } from 'next/font/google';
+
+const geist = Geist({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={geist.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
