734df5c8692c594ba03071f94c8af41aad263b68
rikster-r <seitdanial43@gmail.com>
2025-05-29 21:03:59 +0500

    Change saved data in jwt

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -52,7 +52,9 @@ export default async function handler(
       {
         id: user.id,
         phone_number: user.phone_number,
-        role: user.role,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        photo_url: user.photo_url,
         created_at: user.created_at,
       },
       process.env.JWT_SECRET!,
