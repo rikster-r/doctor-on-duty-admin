e4cbf3eab37f3b6f92689a65b210aadd6a3992c9
rikster-r <seitdanial43@gmail.com>
2025-07-12 18:04:39 +0500

    Fix deployment issues

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  optimizePackageImports: ['@phosphor-icons/react'],
+  experimental: {
+    optimizePackageImports: ['@phosphor-icons/react'],
+  },
 };
 
 export default nextConfig;
